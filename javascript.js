f4b93904b841dbc30a4f4cc6a0f82d8c43332f7e
MauriGM03 <120449991+MauriGM03@users.noreply.github.com>
2023-01-06 00:25:48 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Almacena el enlace original en una variable
+//var originalLink = document.querySelector("a").href;
+
+// Cambia el enlace
+document.querySelector(".a1").href = "https://youtu.be/l60MnDJklnM";
+
+// Reestablece el enlace a su valor original al recargar la página
+//window.onbeforeunload = function() {
+    //document.querySelector("a").href = originalLink;
+//}
